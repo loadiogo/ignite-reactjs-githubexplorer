70ec8a468bdb9633d9c73cb20d05a8c623585c9a
Diogo Henrique <diogo.hglima@gmail.com>
2021-03-30 22:14:17 -0300

    creates a repositoryList api from github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,26 @@
-const respositoryName = 'unform2';
+import { useState, useEffect } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, [repositories]);
+
     return(
         <section className="repository-list">
             <h1>Lista de respositórios</h1>
 
             <ul>
-                <li>
-                    <strong>{respositoryName}</strong>
-                    <p>Forms in React</p>
-                    
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}               
+                
             </ul>
         </section>
     )
